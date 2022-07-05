dc6e124636f542e9f0abd6d95ec4a2d55ab0a444
wanghai <2275981101@qq.com>
2022-07-05 16:46:35 +0800

    update

--- 断墨寻径/docs/.vuepress/enhanceApp.js
+++
@@ -1,9 +0,0 @@
-// 使用异步函数也是可以的
-export default ({
-    Vue, // VuePress 正在使用的 Vue 构造函数
-    router, // 当前应用的路由实例
-    options, // 附加到根实例的一些选项
-    siteData, // 站点元数据
-}) => {
-    // ...做一些其他的应用级别的优化
-}
